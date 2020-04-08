128934a7b9186f4d8b86ade32a1a0c312f67ea4c
Hovhannes1 <h.stepanyan97@gmail.com>
2020-04-09 00:53:23 +0400

    Updates

---
+++ precache-manifest.890c42a6402d1663e1b202f2ec734494.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da3ae744539d1f483d84e691c3fd2416",
+    "url": "/monsters-market/index.html"
+  },
+  {
+    "revision": "3e1933a93d570f109767",
+    "url": "/monsters-market/static/css/main.7508bef9.chunk.css"
+  },
+  {
+    "revision": "baaedbca9fdda8e97a72",
+    "url": "/monsters-market/static/js/2.c0b52254.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/monsters-market/static/js/2.c0b52254.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e1933a93d570f109767",
+    "url": "/monsters-market/static/js/main.6ae81298.chunk.js"
+  },
+  {
+    "revision": "9d683b8015615536ae47",
+    "url": "/monsters-market/static/js/runtime-main.89702bed.js"
+  }
+]);
